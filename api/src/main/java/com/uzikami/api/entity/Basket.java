8547a2f6ff2fa75583ecb3a0cf5642182fdec80c
Uzikami <baohoaitran1502@gmail.com>
2024-06-05 20:12:22 +0700

    av

--- api/src/main/java/com/uzikami/api/entity/Basket.java
+++ api/src/main/java/com/uzikami/api/entity/Basket.java
@@ -1,2 +1,28 @@
-package com.uzikami.api.entity;public class Basket {
+package com.uzikami.api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name="basket")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Basket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "buyer_id")
+    private String buyerId;
+    public Basket(String buyerId) {
+        this.buyerId = buyerId;
+    }
+
 }
+
--- api/src/main/java/com/uzikami/api/entity/BasketItem.java
+++ api/src/main/java/com/uzikami/api/entity/BasketItem.java
@@ -1,2 +1,25 @@
-package com.uzikami.api.entity;public class BasketItem {
+package com.uzikami.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table (name="basket_item")
+@Getter
+@Setter
+@ToString
+public class BasketItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+    @OneToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Product product;
+
 }
